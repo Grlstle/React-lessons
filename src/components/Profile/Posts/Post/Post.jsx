1e639dcb9e79f5c55d83b95e1dd9d5062b3cd46f
Gristie <ryletd@gmail.com>
2021-03-10 01:52:22 +0300

    idk

--- src/components/Profile/Posts/Post/Post.jsx
+++ src/components/Profile/Posts/Post/Post.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import cls from './Post.module.css'
 
-function Post() {
+function Post(props) {
     return (
         <div className={cls.post}>
             <div className={cls.img__wrapper}>
                 <img 
                     className={cls.post__img}
-                    src="https://fk.pythonanywhere.com/ujv"
+                    src={props.img}
                 />
             </div>
             <div className={cls.post__title}>
-                Fname Lname
+                {props.name}
             </div>
             <div className={cls.post__text}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil eos adipisci magnam perferendis asperiores! Facilis ut perferendis maiores nostrum, recusandae labore, similique ducimus commodi est quae, esse quos tempora possimus!
+                {props.text}
             </div>
         </div>
     );
--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -6,12 +6,36 @@ import Post from './Post/Post'
 function Posts() {
     return (
         <div className={cls.posts}>
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
+            <Post 
+                img="https://fk.pythonanywhere.com/ujv"
+                name="Кондратьев Ян Тимурович"
+                text="Lorem ipsum dolor sit"
+            />
+            <Post 
+                img="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Lil_Pump%27s_mugshot.jpg/170px-Lil_Pump%27s_mugshot.jpg"
+                name="Юдин Клим Егорович"
+                text="Amet consectetur adipisicing elit."    
+            />
+            <Post 
+                img='https://resources.tidal.com/images/07738a80/a068/4ef7/8311/d378def4fef9/750x750.jpg'
+                name="Богданов Власий Рудольфович"
+                text="Nihil eos adipisci magnam perferendis asperiores!"
+            />
+            <Post 
+                img="https://consequenceofsound.net/wp-content/uploads/2020/09/lil-peep-hellboy-available-streaming-services.jpg?quality=80"
+                name="Марков Любовь Созонович"
+                text="Facilis ut perferendis maiores nostrum"
+            />
+            <Post 
+                img="https://static.onecms.io/wp-content/uploads/sites/20/2020/04/21/lil-xan.jpg"
+                name="Герасимов Пантелей Егорович"
+                text="R ecusandae labore, similique ducimus commodi est quae"
+            />
+            <Post 
+                img="https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2018%2F02%2Flil-uzi-vert-dj-premiere-kanye-west-robocop-ebro-00.jpg?w=960&cbr=1&q=90&fit=max"
+                name="Копылов Митрофан Авксентьевич"
+                text="Esse quos tempora possimus!"
+            />
         </div>
     );
 }
