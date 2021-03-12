5a2d3b5c7233654e56dc9d0a6b39cdf03b06ccba
Gristie <ryletd@gmail.com>
2021-03-13 01:17:31 +0300

    21 Done

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -3,8 +3,34 @@ import cls from './Messages.module.css'
 
 function Messages(props){
     return(
-        <div>
-            Messages
+        <div className={cls.dialogs}>
+            <div className={cls.dialogs__items}>
+                <div className={`${cls.dialogs__item} ${cls.active}`}>
+                    Dima
+                </div>
+                <div className={cls.dialogs__item}>
+                    Andrey
+                </div>
+                <div className={cls.dialogs__item}>
+                    Jora
+                </div>
+                <div className={cls.dialogs__item}>
+                    Igor
+                </div>
+                <div className={cls.dialogs__item}>
+                    Sasha
+                </div>
+                <div className={cls.dialogs__item}>
+                    David
+                </div>
+            </div>
+            <div className={cls.messages}>
+                <div className={cls.message}>Hello</div>
+                <div className={cls.message}>Hi</div>
+                <div className={cls.message}>Hey</div>
+                <div className={cls.message}>Hola</div>
+                <div className={cls.message}>Yo</div>
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,44 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import cls from './Navbar.module.css'
 
 function Navbar() {
     return (
         <nav className={cls.nav}>
             <div className={cls.nav__item}>
-                <a href="/Profile">Profile</a>
+                <NavLink 
+                    to="/Profile"
+                    activeClassName={cls.selected}>
+                    Profile
+                </NavLink>
             </div>
             <div className={cls.nav__item}>
-                <a href="/Messages"> Messages</a>
+                <NavLink
+                    to="/Messages"
+                    activeClassName={cls.selected}>
+                    Messages
+                </NavLink>
             </div>
             <div className={cls.nav__item}>
-                <a href="/News">News</a>
+                <NavLink 
+                    to="/News"
+                    activeClassName={cls.selected}>
+                    News
+                </NavLink>
             </div>
             <div className={cls.nav__item}>
-                <a href="/Music">Music</a>
+                <NavLink 
+                    to="/Music"
+                    activeClassName={cls.selected}>
+                    Music
+                </NavLink>
             </div>
             <div className={cls.nav__item}>
-                <a href="/Settings">Settings</a>
+                <NavLink
+                    to="/Settings"
+                    activeClassName={cls.selected}>
+                    Settings
+                </NavLink>
             </div>
         </nav>
     );
